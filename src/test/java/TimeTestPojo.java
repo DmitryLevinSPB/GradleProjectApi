ce4a2355201f744b0ac8663ab7c42a9a161862ee
Левины <Levushka22@yandex.ru>
2024-12-02 18:50:54 +0300

    add

---
+++ src/test/java/TimeTestPojo.java
@@ -0,0 +1,17 @@
+public class TimeTestPojo{
+	private String name;
+	private String job;
+	private String updatedAt;
+
+	public String getName(){
+		return name;
+	}
+
+	public String getJob(){
+		return job;
+	}
+
+	public String getUpdatedAt(){
+		return updatedAt;
+	}
+}
